70a2efe57b4d78576d1621f41b68948758f3f14d
Abhi <meabhishek1512@gmail.com>
2024-03-14 14:35:45 +0530

    created work component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import Work from "./components/Work"
 
 const App = () => {
     return (
         <div className="w-full h-screen bg-zinc-900 text-white">
             <Navbar />
+            <Work />
         </div>
     )
 }
