fbb286343fe6f11502b13719352caaa1d9665ed4
Abhi <meabhishek1512@gmail.com>
2024-03-14 13:20:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+    return <div className="w-full h-screen bg-zinc-900 text-white">App</div>
+}
+
+export default App
