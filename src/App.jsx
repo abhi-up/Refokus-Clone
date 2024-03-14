264507f53b25ab4f4c5f3189d0128ca73e240ecf
Abhi <meabhishek1512@gmail.com>
2024-03-14 13:22:28 +0530

    created navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react"
 import Navbar from "./components/Navbar"
 
 const App = () => {
-    return <div className="w-full h-screen bg-zinc-900 text-white">App</div>
+    return (
+        <div className="w-full h-screen bg-zinc-900 text-white">
+            <Navbar />
+        </div>
+    )
 }
 
 export default App
