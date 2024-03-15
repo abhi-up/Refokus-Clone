10cd2d6c58b3ae9fdc5f675aee1ea3a2f329cc59
Abhi <meabhishek1512@gmail.com>
2024-03-15 13:48:54 +0530

    products animation done with framer-motion

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,10 +1,13 @@
 import React from "react"
 import Button from "../shared/Button"
 
-const Product = ({ val }) => {
+const Product = ({ val, mover, count }) => {
     return (
-        <div className="w-full py-20 text-white">
-            <div className="max-w-screen-xl mx-auto flex items-center justify-between">
+        <div className="w-full h-[20rem] py-20 text-white">
+            <div
+                onMouseEnter={() => mover(count)}
+                className="max-w-screen-xl mx-auto flex items-center justify-between"
+            >
                 <h1 className="text-5xl font-semibold">{val.title}</h1>
                 <div className="dets w-1/3">
                     <p className="mb-10">{val.description}</p>
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,5 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import Product from "./Product"
+import { motion } from "framer-motion"
+
+import arqitel from "../../../public/videos/arqitel.webm"
+import ttr from "../../../public/videos/ttr.webm"
+import yir from "../../../public/videos/yir.webm"
+import yahoo from "../../../public/videos/yahoo.webm"
 
 const Products = () => {
     const products = [
@@ -32,11 +38,72 @@ const Products = () => {
             case: true,
         },
     ]
+
+    const [pos, setPos] = useState(null)
+
+    const mover = (val) => {
+        setPos(val * 20)
+    }
+
     return (
-        <div className="mt-32">
+        <div className="mt-32 relative">
             {products.map((item, index) => (
-                <Product key={index} val={item} />
+                <Product key={index} val={item} mover={mover} count={index} />
             ))}
+            <div className="absolute top-0 w-full h-full pointer-events-none">
+                <motion.div
+                    initial={{ y: pos, x: "-50%" }}
+                    animate={{ y: pos + `rem` }}
+                    transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.6 }}
+                    className="window absolute w-[30rem] h-[20rem] bg-white left-[44%] overflow-hidden"
+                >
+                    <motion.div
+                        animate={{ y: -pos + `rem` }}
+                        transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+                        className=" w-full h-full bg-zinc-900"
+                    >
+                        {pos === 0 && (
+                            <video autoPlay muted loop>
+                                <source src={arqitel} type="video/webm" />
+                                Your browser does not support the video tag.
+                            </video>
+                        )}
+                    </motion.div>
+
+                    <motion.div
+                        animate={{ y: -pos + `rem` }}
+                        transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+                        className=" w-full h-full"
+                    >
+                        <video autoPlay muted loop>
+                            <source src={ttr} type="video/webm" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </motion.div>
+
+                    <motion.div
+                        animate={{ y: -pos + `rem` }}
+                        transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+                        className=" w-full h-full"
+                    >
+                        <video autoPlay muted loop>
+                            <source src={yir} type="video/webm" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </motion.div>
+
+                    <motion.div
+                        animate={{ y: -pos + `rem` }}
+                        transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+                        className=" w-full h-full "
+                    >
+                        <video autoPlay muted loop>
+                            <source src={yahoo} type="video/webm" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </motion.div>
+                </motion.div>
+            </div>
         </div>
     )
 }
