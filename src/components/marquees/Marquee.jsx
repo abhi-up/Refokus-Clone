399f579eb7916f603c45b54114fa28fe101c849e
Abhi <meabhishek1512@gmail.com>
2024-03-15 14:30:32 +0530

    marquee component animation done using framer-motion

--- src/components/marquees/Marquee.jsx
+++ src/components/marquees/Marquee.jsx
@@ -1,14 +1,30 @@
 import React from "react"
+import { motion } from "framer-motion"
 
-const Marquee = ({ imagesUrl }) => {
+const Marquee = ({ imagesUrl, direction }) => {
     return (
-        <div className="flex w-full py-8 gap-20 whitespace-nowrap overflow-hidden">
-            {imagesUrl.map((url, index) => (
-                <img key={index} src={url} className="flex-shrink-0 w-[6vw]" />
-            ))}
-            {imagesUrl.map((url, index) => (
-                <img key={index} src={url} className="flex-shrink-0 w-[6vw]" />
-            ))}
+        <div className="flex w-full overflow-hidden">
+            <motion.div
+                initial={{ x: direction === "left" ? "0" : "-100%" }}
+                animate={{ x: direction === "left" ? "-100%" : "0" }}
+                transition={{ ease: "linear", duration: 20, repeat: Infinity }}
+                className="flex flex-shrink-0 gap-20 py-8 pr-24"
+            >
+                {imagesUrl.map((url, index) => (
+                    <img key={index} src={url} className="w-[8vw]" />
+                ))}
+            </motion.div>
+
+            <motion.div
+                initial={{ x: direction === "left" ? "0" : "-100%" }}
+                animate={{ x: direction === "left" ? "-100%" : "0" }}
+                transition={{ ease: "linear", duration: 20, repeat: Infinity }}
+                className="flex flex-shrink-0 gap-20 py-8 pr-24"
+            >
+                {imagesUrl.map((url, index) => (
+                    <img key={index} src={url} className="w-[8vw]" />
+                ))}
+            </motion.div>
         </div>
     )
 }
--- src/components/marquees/Marquees.jsx
+++ src/components/marquees/Marquees.jsx
@@ -11,8 +11,8 @@ const Marquees = () => {
             "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2756e8df24e52915c71_flowcode.svg",
             "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275e1d5aa08ccc379dd_ypo.svg",
             "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275909535ff431975cf_lavender.svg",
-            "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d36963b956910ca67534_remind.svg",
             "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2ce14e6c85b010c2e1e3c_Logo%20White%201.svg",
+            "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d36963b956910ca67534_remind.svg",
         ],
         [
             "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275a4f001b08a4e28bc_weglot.svg",
@@ -28,9 +28,13 @@ const Marquees = () => {
         ],
     ]
     return (
-        <div className="mt-32">
+        <div className="py-20 mt-20 w-full relative overflow-hidden">
             {images.map((item, index) => (
-                <Marquee key={index} imagesUrl={item} />
+                <Marquee
+                    key={index}
+                    direction={index === 0 ? "left" : "right"}
+                    imagesUrl={item}
+                />
             ))}
         </div>
     )
